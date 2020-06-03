46364a81b04d646d92cd2d471d39fb7b7aa265bd
PunithaCP <punithacp95@gmail.com>
2020-06-03 12:03:24 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { MatIconRegistry } from '@angular/material';
+
+
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule, MatNativeDateModule,MatCheckboxModule,MatSelectModule, MatInputModule, MatIconModule, MatSnackBarModule, MatSidenavModule, MatProgressBarModule, MatListModule, MatTooltipModule } from '@angular/material';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from "./app.component";
-import { SearchComponent } from './search/search.component';
-import { RouterModule } from "@angular/router";
-import { AdvanceSearchPageComponent } from './advance-search-page/advance-search-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 import { DatePickerComponent } from './date-picker/date-picker.component';
+import { AdvanceSearchPageComponent } from './advance-search-page/advance-search-page.component';
+import { SearchComponent } from './search/search.component';
 import { TestComponent } from './test/test.component';
 import { Test1Component } from './test1/test1.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot([
-    { path: 'search', component: SearchComponent }
-  ])],
-  declarations: [AppComponent, SearchComponent, AdvanceSearchPageComponent, DatePickerComponent, TestComponent, Test1Component],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule,MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSelectModule, MatInputModule,BrowserAnimationsModule,MatIconModule, MatSnackBarModule, ReactiveFormsModule, ReactiveFormsModule, MatSidenavModule, MatProgressBarModule, MatListModule, MatTooltipModule, 
+  RouterModule.forRoot([
+      { path: '', component: SearchPageComponent },
+      { path: 'advanceSearch', component: AdvanceSearchPageComponent},
+      { path: 'search', component: SearchPageComponent}
+    ])],
+  declarations: [AppComponent, SearchPageComponent, DatePickerComponent, AdvanceSearchPageComponent, SearchComponent, TestComponent, Test1Component],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
